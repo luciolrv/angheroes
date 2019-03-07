45c2245f9054a2aab04f6add8cc553397b35dfef
luciolrv <lucio.impa.br>
2019-03-06 23:38:07 -0300

    change to heroes definition of heroes coming from mock-heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+
+import * as hero from './mock-heroes';
+
+//heroes = hero.HEROES; // use the class
+//heroes = HEROES; // use the class?
+
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +17,10 @@ import { HEROES } from './mock-heroes';
 
 export class HeroService {
 
-  constructor() { }
-  getHeroes(): Hero[] {
-    return HEROES;
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
   }
 }
